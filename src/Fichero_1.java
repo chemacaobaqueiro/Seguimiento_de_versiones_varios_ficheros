5e9b4f9e01f27441fc2808fc418e2f6345cf9f85
Chema <jcaobaqueiro@danielcastelao.org>
2023-09-29 09:46:27 +0200

    Quinto commit, modificacion en los dos primeros ficheros

--- src/Fichero_1.java
+++ src/Fichero_1.java
@@ -1,3 +1,4 @@
 public class Fichero_1 {
     //primer cambio en solo un fichero
+    System.out.println("Cambios solo en los dos primero ficheros");
 }
--- src/Fichero_2.java
+++ src/Fichero_2.java
@@ -1,3 +1,4 @@
 public class Fichero_2 {
     System.out.println("Cambio en el segundo fichero");
+    System.out.println("Cambio solo en los dos primeros ficheros");
 }
