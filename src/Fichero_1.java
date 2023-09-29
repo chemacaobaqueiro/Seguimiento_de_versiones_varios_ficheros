5fda3d410ccc22d9e7298c3efa89103243f0529b
Chema <jcaobaqueiro@danielcastelao.org>
2023-09-29 09:48:28 +0200

    Sexto commit, cambios en el primer y tercer fichero

--- src/Fichero_1.java
+++ src/Fichero_1.java
@@ -1,4 +1,5 @@
 public class Fichero_1 {
     //primer cambio en solo un fichero
     System.out.println("Cambios solo en los dos primero ficheros");
+    System.out.println("Cambios en el primer y tercer fichero");
 }
--- src/Fichero_3.java
+++ src/Fichero_3.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Fichero 3");
+        System.out.println("Cambios en el primer y tercer fichero");
 
         }
     }
